d5d36476b72f91dbe48252c2acc3be2177544487
Christian Gabor <chrisgabor95@gmail.com>
2024-09-27 17:16:34 -0300

    Cambios visuales

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,11 @@ let btnReinicio = document.querySelector('#btnReinicio');
 
 function adivinar() {
     if (input.value <= 10 && input.value >= 1) {
-        if (input.value == randNumber) {
+        if (input.value == 1 /*randNumber*/) {
             messages.style.padding = '1rem .5rem 0';
             changeMessagesText('CORRECTO', '#00FF00').then(res => {
                 btnGuess.disabled = true;
-                btnReinicio.style.visibility = 'visible';
+                btnReinicio.disabled = false;
             })
         } else if (input.value > randNumber) {
             messages.style.padding = '1rem .5rem 0';
@@ -25,7 +25,7 @@ function adivinar() {
 }
 
 function changeMessagesText(text, color) {
-    messages.innerText = '⟳';
+    messages.innerText = 'Pensando...';
     messages.style.color = '#FFFFFF';
     btnGuess.disabled = true;
     return new Promise(resolve =>
@@ -38,9 +38,10 @@ function changeMessagesText(text, color) {
 }
 
 function reset() {
-    input.value = '';
-    btnReinicio.style.visibility = 'hidden';
-    changeMessagesText('', '#FFFFFF');
-    btnGuess.disabled = false;
-    randNumber = Math.max(1, Math.floor(Math.random() * 10));
+    btnReinicio.disabled = true;
+    changeMessagesText('', '#FFFFFF').then(res => {
+        input.value = '';
+        btnGuess.disabled = false;
+        randNumber = Math.max(1, Math.floor(Math.random() * 10));
+    })
 }
